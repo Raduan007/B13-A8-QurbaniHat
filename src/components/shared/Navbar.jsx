734a2f5b1025fd6d5468a1a26ebf0ebc9d24b4da
raduan007 <raduanhossen517@gmail.com>
2026-05-03 00:28:00 +0600

    navbar fix

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -121,14 +121,14 @@ const Navbar = () => {
 
         <div className="p-4 flex flex-col gap-3">
 
-          {/* CLOSE */}
+          {/* Close */}
           <div className="flex justify-end">
             <button onClick={() => setOpen(false)}>
               <FiX size={20} />
             </button>
           </div>
 
-          {/* NAV */}
+          {/* Nav */}
           <Link href="/" onClick={() => setOpen(false)} className="px-2 py-2 hover:bg-gray-800 rounded text-sm">
             Home
           </Link>
