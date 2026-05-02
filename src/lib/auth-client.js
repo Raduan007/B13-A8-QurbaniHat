ca844bc7d148bcc162f04f2ca64852356e593d42
raduan007 <raduanhossen517@gmail.com>
2026-05-02 19:16:48 +0600

    Vercel Bug Fixed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
-    //  baseURL: "https://b13-a8-qurbani-hat.vercel.app"
+    // baseURL: "http://localhost:3000",
+     baseURL: "https://b13-a8-qurbani-hat.vercel.app"
 }) 
 export const {signUp, signIn, SignOut, useSession} = 
 createAuthClient
