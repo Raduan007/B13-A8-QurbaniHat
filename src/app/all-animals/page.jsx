856ef48e4d95d132ca2d5aece8c4293f58968b70
raduan007 <raduanhossen517@gmail.com>
2026-05-02 19:44:38 +0600

    Loader Fixed

--- src/app/all-animals/page.jsx
+++ src/app/all-animals/page.jsx
@@ -1,4 +1,5 @@
 import AnimalCard from "@/components/AnimalCard";
+import PageLoader from "@/components/PageLoader";
 
 const AllAnimalsPage = async () => {
   const res = await fetch(
@@ -9,25 +10,27 @@ const AllAnimalsPage = async () => {
   const animals = await res.json();
 
   return (
-    <div className="px-4 md:px-0">
+    <PageLoader>
+      <div className="px-4 md:px-0">
 
-      <div className="text-center">
-        <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-yellow-500">
-          Find Your Perfect Animal
-        </h1>
+        <div className="text-center">
+          <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-yellow-500">
+            Find Your Perfect Animal
+          </h1>
 
-        <p className="mt-4 opacity-80 text-gray-500">
-          Browse healthy, verified animals for Qurbani and farming needs.
-        </p>
+          <p className="mt-4 opacity-80 text-gray-500">
+            Browse healthy, verified animals for Qurbani and farming needs.
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-8 px-4 md:px-0">
+          {animals.map((animal) => (
+            <AnimalCard key={animal.id} animal={animal} />
+          ))}
+        </div>
+
       </div>
-
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-8 px-4 md:px-0">
-        {animals.map((animal) => (
-          <AnimalCard key={animal.id} animal={animal} />
-        ))}
-      </div>
-
-    </div>
+    </PageLoader>
   );
 };
 
---
+++ src/components/PageLoader.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function PageLoader({ children }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 2 seconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="h-16 w-16 border-4 border-gray-300 border-t-gray-900 rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+
+  return children; add
+}
