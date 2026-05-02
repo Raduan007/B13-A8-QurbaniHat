7ef7d277ddf4789964d35b74ad9fe46234c8cd6d
raduan007 <raduanhossen517@gmail.com>
2026-05-02 22:43:53 +0600

    Update Profile

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,26 @@
+ "use client"
+import { UpdateUserModal } from "@/components/UpdateUserModal";
+ import { authClient } from "@/lib/auth-client";
+import { Avatar, Card } from "@heroui/react";
 
 const ProfilePage = () => {
+     const userData = authClient.useSession();
+      const user = userData.data?.user;
+ console.log(user);
     return (
-        <div>
-            <h1>Profile</h1>
-        </div>
+        
+    <Card className="max-w-96 mx-auto flex flex-col items-center border mt-5">
+         <Avatar className="h-20 w-20">
+           <Avatar.Image src={user?.image} />
+                <Avatar.Fallback>
+                  {user?.name?.charAt(0)}
+                </Avatar.Fallback>
+            </Avatar>
+             <h2 className="text-xl font-bold">{user?.name}</h2>
+             <p className="text-muted">{user?.email}</p>
+             <UpdateUserModal></UpdateUserModal>
+      </Card>
+      
     );
 };
 
