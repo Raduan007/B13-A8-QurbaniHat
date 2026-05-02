21e90d50e1fd8d483008ab293f20bcc2a6339dd9
raduan007 <raduanhossen517@gmail.com>
2026-05-03 01:58:21 +0600

    protected route issue fix

--- proxy.js
+++ proxy.js
@@ -1,16 +1,17 @@
 import { NextResponse } from 'next/server'
 import { auth } from './src/lib/auth'
-import { headers } from 'next/headers'
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request) {
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: request.headers,
   })
 
   if (!session) {
     return NextResponse.redirect(new URL('/signin', request.url))
   }
+
+  return NextResponse.next()
 }
 
 export const config = {
