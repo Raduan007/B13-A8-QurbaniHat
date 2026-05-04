33b6342297d957f7a0060d022e6a9746a6c225e7
raduan007 <raduanhossen517@gmail.com>
2026-05-04 09:49:32 +0600

    Profile Update

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -6,22 +6,27 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const Navbar = () => {
   const userData = authClient.useSession();
   const user = userData.data?.user;
 
   const pathname = usePathname();
+  const router = useRouter();
   const [open, setOpen] = useState(false);
 
+  
   const handleSignOut = async () => {
     await authClient.signOut();
+
+    router.push("/");    
+    router.refresh();    
+    setOpen(false);      
   };
 
   return (
     <div className="w-full text-white bg-gradient-to-r from-gray-900 to-gray-700">
-
       <nav className="flex justify-between items-center px-4 py-2">
 
         {/* logo */}
@@ -30,7 +35,6 @@ const Navbar = () => {
           <h3 className="font-bold text-lg">
             <span className="text-xl md:text-4xl">QurBaniHat</span>
             <span className="text-red-500 text-xl md:text-2xl"> Livestock</span>
-           
           </h3>
         </div>
 
